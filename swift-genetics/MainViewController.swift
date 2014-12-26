7b140b0d901ecf2bbed8ee1d27cff935698a91b3
NachoSoto <ignaciosoto90@gmail.com>
2014-12-26 01:34:44 +0000

    Added Settings to UI

--- swift-genetics/MainViewController.swift
+++ swift-genetics/MainViewController.swift
@@ -14,36 +14,66 @@ class MainViewController: UIViewController {
 		super.init(coder: aDecoder)
 	}
 
+	private var decimalNumberFormatter: NSNumberFormatter = {
+		let formatter = NSNumberFormatter()
+		formatter.numberStyle = .DecimalStyle
+		formatter.maximumFractionDigits = 3
+
+		return formatter
+	}()
+
+	private var iterations: Int = 0
+
 	private var population: Population? {
 		didSet {
 			if let population = population {
 				let size = imageView.bounds.size
 				let scale = view.contentScaleFactor
 
-				println("Fittest: \(population.fittestIndividual.fitness)")
-
 				dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0)) {
 					let image = population.fittestIndividual.dna.imageWithSize(size, scale: scale)
 
 					dispatch_sync(dispatch_get_main_queue()) {
+						self.iterations += 1
+
 						self.imageView.image = image
+						self.currentFitnessLabel.text = "Fitness:\n\(self.decimalNumberFormatter.stringFromNumber(population.fittestIndividual.fitness)!)"
+						self.iterationsLabel.text = "Iterations: \(self.iterations)"
 					}
 				}
 			}
 		}
 	}
 
+	
+	@IBOutlet private var mutationChanceLabel: UILabel!
+	@IBOutlet private var mutationChanceSlider: UISlider!
+	@IBOutlet private var mutationAmountLabel: UILabel!
+	@IBOutlet private var mutationAmountSlider: UISlider!
+	@IBOutlet private var fittestSurvivesSwitch: UISwitch!
+
+	@IBOutlet private var iterationsLabel: UILabel!
+	@IBOutlet private var currentFitnessLabel: UILabel!
+	@IBOutlet private var imageView: UIImageView!
+
 	var referenceImage: UIImage!
 
-	@IBOutlet private var imageView: UIImageView!
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		self.navigationController?.interactivePopGestureRecognizer.enabled = false
+
+		updateSettings()
+	}
 
 	override func viewDidAppear(animated: Bool) {
 		super.viewDidAppear(animated)
 
 		assert(referenceImage != nil, "Reference image was not set")
-
 		startWithPopulation(Population.PopulationForReferenceImage(referenceImage))
 	}
+
+	// MARK: Iteration
 
 	func startWithPopulation(population: Population) {
 		self.population = population
@@ -61,5 +91,24 @@ class MainViewController: UIViewController {
 				self.iterate()
 			}
 		}
+	}
+
+	// MARK: Events
+
+	@IBAction func mutationChanceChanged(sender: UISlider) {
+		updateSettings()
+	}
+
+	@IBAction func mutationAmountChanged(sender: UISlider) {
+		updateSettings()
+	}
+
+	private func updateSettings() {
+		Genetics.settings.mutationChance = mutationChanceSlider.value
+		Genetics.settings.mutationAmount = mutationAmountSlider.value
+		Genetics.settings.fittestSurvives = fittestSurvivesSwitch.enabled
+
+		mutationChanceLabel.text = "Mutation chance: \(self.decimalNumberFormatter.stringFromNumber(Genetics.settings.mutationChance)!)"
+		mutationAmountLabel.text = "Mutation amount: \(self.decimalNumberFormatter.stringFromNumber(Genetics.settings.mutationAmount)!)"
 	}
 }
